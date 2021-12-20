0e21a63c2209f0cb32e8bdb513eed369fcf77183
bart7711 <bart0594@stud.kea.dk>
2021-12-20 14:38:24 +0100

    Initial commit

---
+++ src/test/java/bart/electionsystem/ElectionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package bart.electionsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectionSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
